e24d416394e87dbceb6aff56bd9b7e5270e10aca
Johnny McNeil <johnnymmcneil@gmail.com>
2024-01-22 16:31:57 -0700

    Adding project files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Storyblok Astro Bug
+
+Used for reproducing an issue in @storyblok/astro
+
+(Steps to reproduce coming soon)
---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
+export default defineConfig({
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  },
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page'
+      },
+      apiOptions: {
+        region: 'us'
+      }
+    })
+  ]
+});
